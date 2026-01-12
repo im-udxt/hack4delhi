b07f297e03bd83f98b820d69ea911289a16757be
Udit Garg <uditgarg.dev@gmail.com>
2026-01-12 11:53:57 +0530

    Final h bboss

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Pollution-dasboard/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/hack4delhi/' : '/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
